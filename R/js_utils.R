bdafc45a9b120fb5cfa65b8baba244e512b73f68
Robert Ashton <r.ashton@imperial.ac.uk>
2020-05-07 18:11:04 +0100

    Set filter as focus on page load

--- R/js_utils.R
+++ R/js_utils.R
@@ -5,8 +5,15 @@
 #' @keywords internal
 get_clicked_row_value <- function() {
   htmlwidgets::JS(
-    "table.on('click.dt', 'td', function() {
+    "$('div.dataTables_filter input', table.table().container()).focus();
+
+    table.on('click.dt', 'td', function() {
        var clicked_file = table.row(this).data()[0];
        Shiny.onInputChange('clicked_file', clicked_file);
      });")
+}
+
+set_filter_focus <- function() {
+  htmlwidgets::JS(
+    "$('div.dataTables_filter input', table.table().container()).focus();")
 }
