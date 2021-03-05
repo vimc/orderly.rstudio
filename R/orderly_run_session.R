8a1137b28bddd21dc3319c9f5636e4cb3917fac0
Robert Ashton <r.ashton@imperial.ac.uk>
2021-03-05 09:44:09 +0000

    Update docs and regenerate

--- R/orderly_run_session.R
+++ R/orderly_run_session.R
@@ -1,3 +1,15 @@
+#' Run an orderly report in separate session.
+#'
+#' This will run an orderly report using RStudio jobs. This will run the
+#' report async i.e. it won't block your RStudio terminal. This means
+#' that it returns no info about success of job run - it only tells us
+#' the job has started.
+#'
+#' @inheritParams orderly::orderly_run
+#' @param commit If TRUE then report will be committed to archive after running
+#'
+#' @return Invisible return, called for side effects
+#' @export
 orderly_run_session <- function(name = NULL, parameters = NULL, envir = NULL,
                                 root = NULL, locate = TRUE,
                                 message = NULL, instance = NULL,
@@ -25,4 +37,5 @@ orderly_run_session <- function(name = NULL, parameters = NULL, envir = NULL,
   withr::with_envvar(c("ORDERLY_RUN_ARGS_PATH" = args_path), {
     rstudioapi::jobRunScript(script_path)
   })
+  invisible(TRUE)
 }
