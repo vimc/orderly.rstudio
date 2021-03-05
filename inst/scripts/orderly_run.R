3f15c2ddf7cdfc53e289aa71d4c11ba77c18149f
Robert Ashton <r.ashton@imperial.ac.uk>
2021-03-05 09:36:09 +0000

    Add note about why we need to save params to RDS file

--- inst/scripts/orderly_run.R
+++ inst/scripts/orderly_run.R
@@ -1,12 +1,20 @@
-## Prepare params
+## This script is designed to be run as an RStudio job
+## see orderly_run_session.R which sets up the run
+## RStudio jobs cannot be parameterised
+## https://github.com/rstudio/rstudio/issues/4176
+## so to be able to parameterise the report run we need to save out the
+## params we want to use to some file during the setup then read
+## them from file when running the script.
 args <- readRDS(Sys.getenv("ORDERLY_RUN_ARGS_PATH"))
 
 orderly:::orderly_run_internal(name = args[["name"]],
                                parameters = args[["parameters"]],
-                               envir = args[["envir"]], root = args[["root"]],
+                               envir = args[["envir"]],
+                               root = args[["root"]],
                                locate = args[["locate"]],
                                message = args[["message"]],
                                instance = args[["instance"]],
                                use_draft = args[["use_draft"]],
-                               remote = args[["remote"]], tags = args[["tags"]],
+                               remote = args[["remote"]],
+                               tags = args[["tags"]],
                                commit = args[["commit"]])
